9ddee90e3da545715bc23bb181ac89b2ab169fc5
npsboy <111739622+npsboy@users.noreply.github.com>
2024-05-18 12:24:01 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,126 @@
+/**
+ * 25,
+ * 
+ * 74
+ */
+/**
+ * 100          45
+ * 
+ * 170          57
+ */
+input.onButtonPressed(Button.AB, function () {
+    position = pickup
+    for (let index = 0; index < 11; index++) {
+        if (angle_1 > position) {
+            while (!(angle_1 == position)) {
+                angle_1 += -1
+                basic.pause(20)
+            }
+        } else {
+            if (angle_1 < position) {
+                while (!(angle_1 == position)) {
+                    angle_1 += 1
+                    basic.pause(20)
+                }
+            }
+        }
+        basic.pause(2000)
+        while (!(angle_2 == 170)) {
+            angle_2 += 1
+            basic.pause(20)
+        }
+        basic.pause(1500)
+        angle_3 = 0
+        basic.pause(1000)
+        while (!(angle_2 == 130)) {
+            angle_2 += -1
+            basic.pause(20)
+        }
+        basic.pause(500)
+        while (!(angle_2 == 170)) {
+            angle_2 += 1
+            basic.pause(20)
+        }
+        basic.pause(1500)
+        angle_3 = 50
+        basic.pause(1000)
+        while (!(angle_2 == 100)) {
+            angle_2 += -1
+            basic.pause(20)
+        }
+        basic.pause(2000)
+        if (position == pickup) {
+            position = drop
+        } else {
+            position = pickup
+        }
+    }
+})
+let position = 0
+let angle_3 = 0
+let angle_2 = 0
+let angle_1 = 0
+let drop = 0
+let pickup = 0
+pickup = 72
+drop = 18
+angle_1 = 18
+angle_2 = 100
+angle_3 = 0
+basic.forever(function () {
+    servos.P1.setAngle(angle_3)
+})
+basic.forever(function () {
+    basic.showString("" + (angle_3))
+})
+basic.forever(function () {
+    servos.P0.setAngle(angle_1)
+})
+basic.forever(function () {
+    servos.P2.setAngle(angle_2)
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        if (angle_1 > 0) {
+            angle_1 += -0.5
+        }
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltRight)) {
+        if (angle_1 < 180) {
+            angle_1 += 0.5
+        }
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        if (angle_2 > 100) {
+            angle_2 += -1
+        }
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoDown)) {
+        if (angle_2 < 170) {
+            angle_2 += 1
+        } else {
+            angle_2 = 172
+        }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        angle_3 = 0
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        angle_3 = (angle_2 - 100) / 5.8 + 45
+    }
+})
+basic.forever(function () {
+    if (!(angle_3 == 0)) {
+        angle_3 = (angle_2 - 100) / 5.8 + 45
+    }
+})
